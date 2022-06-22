f9c38d7654e5d0e3e1e28d2f09d0f9124e2e9909
amovfx <45839100+amovfx@users.noreply.github.com>
2022-06-22 14:59:32 -0600

    added tests for encoding and decoding jwts.

--- tests/test_security.py
+++ tests/test_security.py
@@ -5,7 +5,8 @@
 
 from security.generators import AsymmetricKeyGenerator, config
 from pathlib import Path
-
+import jwt
+import pytest
         
         
 class TestAsymetricKeyGenerator:
@@ -13,7 +14,11 @@ class TestAsymetricKeyGenerator:
     """
     
     keys = AsymmetricKeyGenerator(512)
+    
     keys.dump()
+    keys_bad = AsymmetricKeyGenerator(512)
+    payload = {"Test": "Token"}
+    token = jwt.encode(payload=payload, key=keys.get_priv_key(), algorithm="RS256")
     
     def test_cert_dump(self):
 
@@ -23,5 +28,18 @@ class TestAsymetricKeyGenerator:
         assert priv_path.is_file()
         assert pub_path.is_file()
         assert jwk_path.is_file()
+        
+        
+    def test_encode_decode(self): 
+        token = jwt.encode(payload=self.payload, key=self.keys.get_priv_key(), algorithm="RS256")
+        assert jwt.decode(token, self.keys.get_pub_key(), algorithms=["RS256"]) == self.payload
+        
+    def test_bad_key(self):
+        with pytest.raises(jwt.exceptions.InvalidSignatureError):
+            jwt.decode(self.token, self.keys_bad.get_pub_key(), algorithms=["RS256"])
+        
+        
+        
+        
         
 
